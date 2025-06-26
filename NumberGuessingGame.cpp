62ad52511e08fcef0ed5a5e5d67def8c92de224d
Gerose316 <geraldgarciaurena12@gmail.com>
2025-06-26 18:01:06 -0400

    NumberGuessingGame.cpp
    Is a little tiny project from my c++ roadmap backend and trying new things

---
+++ NumberGuessingGame.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+int Easy(){
+    std::cout << "Great! You have selected the Easy difficulty level." << std::endl;
+    std::cout << "Let's start the Game!" << std::endl;
+    int guessChamber;
+    int count = 0;
+    int randomNum = rand() % 101;
+    std::cout << randomNum << std::endl;
+    for(int i = 0; i < 10; i++){
+        std::cout << "Enter your guess:" << std::endl;
+        std::cin >> guessChamber;
+        count++;
+        if( count < 10){
+        if(randomNum > guessChamber){
+            std::cout << "Incorrect! The number is greater than" << guessChamber << std::endl;
+            
+        }else if(randomNum < guessChamber){
+            std::cout << "Incorrect! The number is less than" << guessChamber << std::endl;
+         
+        }else if(randomNum == guessChamber){
+            std::cout << "Congratulations! You guessed the correct number in" << count << "attempts." << std::endl;
+            break;
+
+        }
+
+        }else if( count == 10){
+            std::cout << "the end" << std::endl;
+            break;
+        }
+        
+    }
+    
+
+}
+
+int Medium(){
+    std::cout << "Great! You have selected the Medium difficulty level." << std::endl;
+    std::cout << "Let's start the Game!" << std::endl;
+    int guessChamber;
+    int count = 0;
+    int randomNum = rand() % 101;
+    std::cout << randomNum;
+    for(int i = 0; i < 5; i++){
+        std::cout << "Enter your guess:" << std::endl;
+        std::cin >> guessChamber;
+        count++;
+        if( count < 5){
+        if(randomNum > guessChamber){
+            std::cout << "Incorrect! The number is greater than" << guessChamber << std::endl;
+            
+        }else if(randomNum < guessChamber){
+            std::cout << "Incorrect! The number is less than" << guessChamber << std::endl;
+         
+        }else if(randomNum == guessChamber){
+            std::cout << "Congratulations! You guessed the correct number in" << count << "attempts." << std::endl;
+            break;
+
+        }
+
+        }else if( count == 5){
+            std::cout << "the end" << std::endl;
+            break;
+        }
+        
+    }
+}
+
+int Hard(){
+    std::cout << "Great! You have selected the Hard difficulty level." << std::endl;
+    std::cout << "Let's start the Game!" << std::endl;
+    int guessChamber;
+    int count = 0;
+    int randomNum = rand() % 101;
+    std::cout << randomNum;
+    for(int i = 0; i < 3; i++){
+        std::cout << "Enter your guess:" << std::endl;
+        std::cin >> guessChamber;
+        count++;
+        if( count < 3){
+        if(randomNum > guessChamber){
+            std::cout << "Incorrect! The number is greater than" << guessChamber << std::endl;
+            
+        }else if(randomNum < guessChamber){
+            std::cout << "Incorrect! The number is less than" << guessChamber << std::endl;
+         
+        }else if(randomNum == guessChamber){
+            std::cout << "Congratulations! You guessed the correct number in" << count << "attempts." << std::endl;
+            break;
+
+        }
+
+        }else if( count == 3){
+            std::cout << "the end" << std::endl;
+            break;
+        }
+        
+        
+    }
+
+ 
+}
+
+
+// NEEDS TO BE CSV FILE TRACKING ALL THE HIGHSCORES
+void highScore(int score){
+
+}
+// I need to learn or watch a video for this 
+
+
+
+
+
+
+
+int main(){
+    std::cout << "Welcome to the Number Guessing Game!" << std::endl;
+    std::cout << "I'm thinking of a number between 1 and 100." << std::endl;
+    std::cout << "You have 5 chances to guess the correct number." << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Please select the difficulty level" << std::endl;
+    std::cout << "1. Easy (10 chances)" << std::endl;
+    std::cout << "2. Medium (5 chances)" << std::endl;
+    std::cout << "3. Hard (3 chances)" << std::endl;
+
+    int choices;
+    int attempts;
+
+    std::cout << "Enter your choice: ";
+    while(std::cin >> choices){
+        if(choices == 1){
+            attempts = Easy();
+            break;
+        }else if(choices == 2){
+            attempts = Medium();
+            break;
+
+        }else if(choices == 3){
+            attempts = Hard();
+            break;
+        }
+
+    }
+
+    highScore(attempts);
+
+
+
+
+    
+
+
+
+
+}
